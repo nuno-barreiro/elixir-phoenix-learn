b3d96d7e519c24d99863d4792b4b4c2c33adf3d4
Nuno Barreiro <nuno.barreiro@valtech.com>
2022-08-11 21:42:04 +0000

    define identicon creation workflow

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,30 +4,36 @@ defmodule Identicon do
     Generates a 300x300 identicon based on a given string and saves it to the filesystem
   """
   def create(input) do
-
+    input
+    |> compute_hash
+    |> list_numbers
+    |> pick_color
+    |> build_grid
+    |> convert_grid
+    |> save_image
   end
 
   defp compute_hash(input) do
 
   end
 
-  defp list_numbers do
+  defp list_numbers(input) do
 
   end
 
-  defp pick_color do
+  defp pick_color(input) do
 
   end
 
-  defp build_grid do
+  defp build_grid(input) do
 
   end
 
-  defp convert_grid do
+  defp convert_grid(input) do
 
   end
 
-  defp save_image do
+  defp save_image(input) do
 
   end
 
