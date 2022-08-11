b4fb4cd20d783f94b50a0992c5e38dfb039510f6
Nuno Barreiro <nuno.barreiro@valtech.com>
2022-08-11 21:39:27 +0000

    define needed functions for identicon generation

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,34 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+    Generates a 300x300 identicon based on a given string and saves it to the filesystem
   """
-  def hello do
-    :world
+  def create(input) do
+
   end
+
+  defp compute_hash(input) do
+
+  end
+
+  defp list_numbers do
+
+  end
+
+  defp pick_color do
+
+  end
+
+  defp build_grid do
+
+  end
+
+  defp convert_grid do
+
+  end
+
+  defp save_image do
+
+  end
+
 end
