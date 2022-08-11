deb7236680464586f59103fb80cdb85edc742214
Nuno Barreiro <nuno.barreiro@valtech.com>
2022-08-11 22:27:05 +0000

    implement identicon color picker with tests

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,9 +7,9 @@ defmodule Identicon do
     input
     |> compute_hash
     |> pick_color
-    |> build_grid
-    |> convert_grid
-    |> save_image
+    # |> build_grid
+    # |> convert_grid
+    # |> save_image
   end
 
   @doc """
@@ -28,20 +28,30 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(input) do
+  @doc """
+    Picks the color to use to paint squares
 
+    Example:
+      iex>Identicon.pick_color(%Identicon.Image{hex: [97, 64, 154, 161, 253, 71, 212, 165, 51, 45, 226, 60, 191, 89, 163, 111]})
+      %Identicon.Image{
+        color: {97, 64, 154},
+        hex: [97, 64, 154, 161, 253, 71, 212, 165, 51, 45, 226, 60, 191, 89, 163, 111]
+      }
+  """
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = input) do
+    %Identicon.Image{input | color: { red, green, blue }}
   end
 
-  def build_grid(input) do
+  # def build_grid(input) do
 
-  end
+  # end
 
-  def convert_grid(input) do
+  # def convert_grid(input) do
 
-  end
+  # end
 
-  def save_image(input) do
+  # def save_image(input) do
 
-  end
+  # end
 
 end
